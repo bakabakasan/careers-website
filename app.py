ee0c97c2a53b59429fee22a46139de4fbebe66c4
bakabakasan <94224362+bakabakasan@users.noreply.github.com>
2024-01-27 13:28:56 +0000

    addet html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask #импорт класса фласк
+from flask import Flask, render_template
 
 app = Flask(__name__) #создаем объект класса фласк
 
 @app.route("/") #создание маршрута
 def hello_world(): #функция, которая будет вызвана при запросе к маршруту
-    return "Hello, World!" #возвращение строки
+    return render_template('home.html') 
 
 if __name__ == "__main__": #проверка на запуск самого скрипта 
   app.run(host="0.0.0.0", debug=True)
